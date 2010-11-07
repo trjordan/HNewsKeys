344669e945f977e2036092869357b5aad71ca870
TR Jordan <terral.jordan@gmail.com>
2010-11-06 20:54:23 -0400

    j and k now work.

--- helloworld/content/overlay.js
+++ helloworld/content/overlay.js
@@ -1,22 +1,63 @@
+var state = {
+    current: -1,
+    titles: []
+};
+
 var HelloWorld = {
-    onLoad: function() {
+    onLoad: function(e) {
         if (getBrowser().currentURI.spec !== 'http://news.ycombinator.com/') {
             return;
         }
 
-        var style = 'font-size:70';
-
-        var titles = [];
         var anchors = window.content.document.getElementsByTagName('a');
         for (var i = 0; i < anchors.length; i++) {
             if (anchors[i].parentNode.className === 'title') {
-                titles.push(anchors[i]);
+                state.titles.push(anchors[i]);
             }
         }    
-        for (var i = 0; i < titles.length; i++) {
-            titles[i].setAttribute('style', style);
+    },
+
+    onKeypress: function(e) {
+        var letter = (e.charCode === 13) ? 'ENTER' : String.fromCharCode(e.charCode);
+        switch(letter) {
+        case 'j':
+            HelloWorld.move(1);
+            break;
+        case 'k':
+            HelloWorld.move(-1);
+            break;
+        case 'ENTER':
+            HelloWorld.open();
+            break;
+        case 'o':
+            HelloWorld.openComments();
+            break;
+        default:
+            return;
         }
-        alert('Finished!');
+    },
+
+    move: function(num) {
+        var oldPosition = state.current;
+        var newPosition = oldPosition + num;
+        if (newPosition < 0 || newPosition >= state.titles.length) {
+            return;
+        }
+        HelloWorld.embolden(newPosition);
+        state.current = newPosition;
+
+        if (oldPosition < 0 || oldPosition >= state.titles.length) {
+            return;
+        }
+        HelloWorld.unembolden(oldPosition);
+    },
+    
+    unembolden: function(index) {
+        state.titles[index].setAttribute('style', '');
+    },
+
+    embolden: function(index) {
+        state.titles[index].setAttribute('style', 'font-size:24');
     },
 
     onMenuItemCommand: function() {
@@ -24,4 +65,6 @@ var HelloWorld = {
     }
 };
 
-window.addEventListener("DOMContentLoaded", function(e) { HelloWorld.onLoad(e); }, false); 
+window.addEventListener("DOMContentLoaded", HelloWorld.onLoad, false); 
+window.addEventListener("keypress", HelloWorld.onKeypress, false); 
+
