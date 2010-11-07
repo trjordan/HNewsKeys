a20a67bb26dd8aa64a68cf954d55ead233006ff3
TR Jordan <terral.jordan@gmail.com>
2010-11-06 22:20:11 -0400

    Refactored into object(s).

--- helloworld/content/overlay.js
+++ helloworld/content/overlay.js
@@ -1,88 +1,112 @@
-var state = {
-    current  : -1,
-    titles   : [],
-    comments : []
-};
+var HNewsKeys = {
+    getMainPageObj: function() {
+        if (!window.content.HNewsKeys) {
+            window.content.HNewsKeys = new HNewsMainPage();
+        } 
+        return window.content.HNewsKeys;
+    },
 
-var HelloWorld = {
     onLoad: function(e) {
-        if (getBrowser().currentURI.spec !== 'http://news.ycombinator.com/') {
-            return;
+        if (getBrowser().currentURI.spec === 'http://news.ycombinator.com/') {
+            // No!
         }
-
-        var anchors = window.content.document.getElementsByTagName('a');
-        for (var i = 0; i < anchors.length; i++) {
-            if (anchors[i].parentNode.className === 'title') {
-                state.titles.push(anchors[i]);
-            }
-            if (anchors[i].parentNode.className === 'subtext' && 
-                anchors[i].getAttribute('href').slice(0, 4) === 'item') {
-                state.comments.push(anchors[i]);
-            }
-        }    
     },
 
     onKeypress: function(e) {
-        if (getBrowser().currentURI.spec !== 'http://news.ycombinator.com/') {
-            return;
+        if (getBrowser().currentURI.spec === 'http://news.ycombinator.com/') {
+            var obj = HNewsKeys.getMainPageObj();
+            obj.onKeypress(e);
         }
-        var letter = (e.keyCode == 13) ? 'ENTER' : String.fromCharCode(e.charCode);
-        switch(letter) {
-        case 'j':
-            HelloWorld.move(1);
-            break;
-        case 'k':
-            HelloWorld.move(-1);
-            break;
-        case 'ENTER':
-            HelloWorld.open();
-            break;
-        case 'o':
-            HelloWorld.openComments();
-            break;
-        default:
-            return;
-        }
-    },
-
-    open: function() {
-        var url = state.titles[state.current].getAttribute('href');
-        window.content.location = url;
-    },
-
-    openComments: function() {
-        var url = 'http://news.ycombinator.com/' + state.comments[state.current].getAttribute('href');
-        window.content.location = url;
-    },
-
-    move: function(num) {
-        var oldPosition = state.current;
-        var newPosition = oldPosition + num;
-        if (newPosition < 0 || newPosition >= state.titles.length) {
-            return;
-        }
-        HelloWorld.embolden(newPosition);
-        state.current = newPosition;
-
-        if (oldPosition < 0 || oldPosition >= state.titles.length) {
-            return;
-        }
-        HelloWorld.unembolden(oldPosition);
-    },
-    
-    unembolden: function(index) {
-        state.titles[index].setAttribute('style', '');
-    },
-
-    embolden: function(index) {
-        state.titles[index].setAttribute('style', 'font-size:24');
-    },
-
-    onMenuItemCommand: function() {
-        window.open("chrome://helloworld/content/hello.xul", "", "chrome");
     }
 };
 
-window.addEventListener("DOMContentLoaded", HelloWorld.onLoad, false); 
-window.addEventListener("keypress", HelloWorld.onKeypress, false); 
+var HNewsComments = {
+    onLoad: function() {
+
+    }
+};
+
+var HNewsMainPage = function() {
+
+    var current  = -1;
+    var titles   = [];
+    var comments = [];
+
+    function open() {
+        var url = titles[current].getAttribute('href');
+        window.content.location = url;
+    }
+
+    function openComments() {
+        var url = 'http://news.ycombinator.com/' + comments[current].getAttribute('href');
+        window.content.location = url;
+    }
+
+    function move(num) {
+        var oldPosition = current;
+        var newPosition = oldPosition + num;
+        if (newPosition < 0 || newPosition >= titles.length) {
+            return;
+        }
+        embolden(newPosition);
+        current = newPosition;
+
+        if (oldPosition < 0 || oldPosition >= titles.length) {
+            return;
+        }
+        unembolden(oldPosition);
+    }
+    
+    function unembolden(index) {
+        titles[index].setAttribute('style', '');
+    }
+
+    function embolden(index) {
+        titles[index].setAttribute('style', 'font-size:24');
+    }
+
+    var anchors = window.content.document.getElementsByTagName('a');
+    for (var i = 0; i < anchors.length; i++) {
+        if (anchors[i].parentNode.className === 'title') {
+            titles.push(anchors[i]);
+        }
+        if (anchors[i].parentNode.className === 'subtext' && 
+            anchors[i].getAttribute('href').slice(0, 4) === 'item') {
+            comments.push(anchors[i]);
+        }
+    }
+    for (i = 0; i < titles.length; i++) {
+        unembolden(i);
+    }
+    if (current >= 0) {
+        embolden(current);
+    }
+    alert('Finished');
+ 
+    return {
+        onKeypress: function(e) {
+            var letter = (e.keyCode == 13) ? 'ENTER' : String.fromCharCode(e.charCode);
+            switch(letter) {
+            case 'j':
+                move(1);
+                break;
+            case 'k':
+                move(-1);
+                break;
+            case 'ENTER':
+                open();
+                break;
+            case 'o':
+                openComments();
+                break;
+            default:
+                return;
+            }
+        },
+
+    };
+};
+
+window.addEventListener("DOMContentLoaded", HNewsKeys.onLoad, false); 
+window.addEventListener("keypress", HNewsKeys.onKeypress, false); 
 
